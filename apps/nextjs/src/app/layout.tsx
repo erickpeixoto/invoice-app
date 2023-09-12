01e8a8e2dae445838263c3d02746348cc1656b3a
Erick Eduardo <erickepeixoto@gmail.com>
2023-09-12 14:05:52 -0300

    chore: add Clerk as an Auth Service

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -4,7 +4,9 @@ import { Inter } from "next/font/google";
 import "~/styles/globals.css";
 
 import { headers } from "next/headers";
+import { ClerkProvider } from "@clerk/nextjs";
 
+import SideBar from "~/components/SideBar";
 import { TRPCReactProvider } from "./providers";
 
 const fontSans = Inter({
@@ -13,13 +15,13 @@ const fontSans = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Create T3 Turbo",
+  title: "aiWave",
   description: "Simple monorepo with shared backend for web & mobile apps",
   openGraph: {
-    title: "Create T3 Turbo",
+    title: "aiWave",
     description: "Simple monorepo with shared backend for web & mobile apps",
     url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
+    siteName: "aiWave",
   },
   twitter: {
     card: "summary_large_image",
@@ -31,10 +33,18 @@ export const metadata: Metadata = {
 export default function Layout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>{/* Additional <head> elements can be added here. */}</head>
       <body className={["font-sans", fontSans.variable].join(" ")}>
-        <TRPCReactProvider headers={headers()}>
-          {props.children}
-        </TRPCReactProvider>
+        <div className="flex h-screen flex-col bg-gray-100 md:flex-row">
+          <div className="shadow-r w-full bg-white md:h-screen md:w-64">
+            <SideBar />
+          </div>
+          <div className="flex-1 overflow-y-auto p-4 md:p-10">
+            <TRPCReactProvider headers={headers()}>
+              <ClerkProvider>{props.children}</ClerkProvider>
+            </TRPCReactProvider>
+          </div>
+        </div>
       </body>
     </html>
   );
