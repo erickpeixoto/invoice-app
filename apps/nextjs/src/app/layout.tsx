463f74578ccb4c21085c69d0dc0dbec409be98de
Erick Eduardo <erickepeixoto@gmail.com>
2023-09-15 13:23:20 -0300

    chore: ignore migrations

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { Toaster } from "~/components/ui/toaster";
+
 import "~/styles/globals.css";
 
 import { headers } from "next/headers";
@@ -25,6 +27,7 @@ export default function Layout(props: { children: React.ReactNode }) {
     <html lang="en">
       <head>{/* Additional <head> elements can be added here. */}</head>
       <body className={["font-sans", fontSans.variable].join(" ")}>
+        <Toaster />
         <div className="flex h-screen flex-col bg-blue-100 p-5 md:flex-row">
           <SideBar />
           <main className="flex-1 overflow-y-auto bg-gray-100 p-4 md:p-10">
