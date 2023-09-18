35ef3e9b6fcb953192a3699c497b46954209ed31
Erick Eduardo <erickepeixoto@gmail.com>
2023-09-17 21:40:49 -0300

    fix: adjust lint complaints

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,11 +1,8 @@
-import Discord from "@auth/core/providers/discord";
 import type { DefaultSession } from "@auth/core/types";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 
 import { db, tableCreator } from "@acme/db";
-
-import { env } from "./env.mjs";
 
 export type { Session } from "next-auth";
 
@@ -27,12 +24,7 @@ export const {
   CSRF_experimental,
 } = NextAuth({
   adapter: DrizzleAdapter(db, tableCreator),
-  providers: [
-    Discord({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-  ],
+  providers: [],
   callbacks: {
     session: ({ session, user }) => ({
       ...session,
