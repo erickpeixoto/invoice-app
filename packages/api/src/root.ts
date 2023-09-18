a6386d434fc434159813590a3e0d781f665fc167
Erick Eduardo <erickepeixoto@gmail.com>
2023-09-18 09:07:02 -0300

    feat: adjust on TRPC module

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -2,6 +2,7 @@ import { authRouter } from "./router/auth";
 import { clientRouter } from "./router/client";
 import { invoiceRouter } from "./router/invoice";
 import { postRouter } from "./router/post";
+import { userRouter } from "./router/user";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
@@ -9,6 +10,7 @@ export const appRouter = createTRPCRouter({
   post: postRouter,
   invoice: invoiceRouter,
   costumer: clientRouter,
+  users: userRouter,
 });
 
 // export type definition of API
